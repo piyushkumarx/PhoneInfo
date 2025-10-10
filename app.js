b6c2dff609ba34c2b127ce03eade8c78b9cc5bff
Piyush Kushwah <127136125+piyushkumarx@users.noreply.github.com>
2025-10-10 19:18:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ let newvalue = Number(inpt.value);
 if (!isNaN(newvalue) && typeof newvalue === "number") {
   result.innerText = "🌀 Data nikal raha hoon bhai ruk ja...";
   
-  let url = `https://osint-lime.vercel.app/api/hello?key=FUCKDEMO&type=number&term=${newvalue}`;
+  let url = `http://dark-op.dev-is.xyz/?key=wasdark&number=${newvalue}`;
 
 async function data() {
   try {
